194ec890bc6bd0a474cc0bac31706711f22e6cea
Nikki Chen <113538655+itsnkc@users.noreply.github.com>
2022-09-26 16:19:21 +0800

    Add files via upload

---
+++ iris_classification.py
@@ -0,0 +1,71 @@
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+df = pd.read_csv(url, names=names)
+
+# Dataset
+print(df.head(10))
+print(df.tail(10))
+
+print(df.describe())
+print(df.info())
+print(df.shape)
+print(df.groupby('class').size())
+
+sns.pairplot(df, hue="class", height=2, palette='husl')
+
+# Split-out validation dataset
+array = df.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+scoring = 'accuracy'
+
+# Models
+models = []
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('LR', LogisticRegression()))
+models.append(('KNN', KNeighborsClassifier()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %.4f (%.4f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# Make predictions on validation dataset
+model = LinearDiscriminantAnalysis()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+print('線性區別分析LDA')
+print("%.4f" % accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print(model)
+
+# Make predictions on validation dataset
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+print('Logistic迴歸分析')
+print("%.4f" % accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print(model)
+
+# Make predictions on validation dataset
+model = KNeighborsClassifier()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+print('KNN最近鄰近法')
+print("%.4f" % accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print(model)
